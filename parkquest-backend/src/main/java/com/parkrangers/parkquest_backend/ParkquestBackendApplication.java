fe19d60a870bebb7cba693f90065a5facbc139a1
sheetalkkharat <sheetalkharatk@gmail.com>
2025-03-15 09:57:49 -0500

    Cosmetic Changes1

--- parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/ParkquestBackendApplication.java
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/ParkquestBackendApplication.java
@@ -1,4 +1,4 @@
-package com.parkrangers.parkquest_backend.models;
+package com.parkrangers.parkquest_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
