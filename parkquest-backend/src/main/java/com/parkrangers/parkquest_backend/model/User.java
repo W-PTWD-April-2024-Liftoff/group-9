1b2e412051d7ad2b997acedb217524d76b978e57
Rachel Allen <rqllen@gmail.com>
2025-03-20 11:20:46 -0500

    pausing this

---
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/model/User.java
@@ -0,0 +1,96 @@
+package com.parkrangers.parkquest_backend.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Data
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = {"username", "email"}))
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max=20)
+    private String username;
+
+    @NotBlank
+    @Size(max=75)
+    @Email
+    private String email;
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+
+    @NotBlank
+    @Size(min=8, max=20)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.parkrangers.parkquest_backend.repositories;
+
+import com.parkrangers.parkquest_backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+//    @Query("SELECT u FROM User u WHERE u.username = :username")
+//    public User getUserByUsername(@Param("username") String username);
+}
