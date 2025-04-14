517a4280aa32b8e99059cb83c76731905b4ce9f6
Rachel Allen <166883143+rachelal1en@users.noreply.github.com>
2025-04-13 19:40:58 -0500

    Update SubscriptionController.java

--- parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/controller/SubscriptionController.java
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/controller/SubscriptionController.java
@@ -1,16 +1,20 @@
 package com.parkrangers.parkquest_backend.controller;
 
-import com.parkrangers.parkquest_backend.model.request.SubscriptionRequest;
+import com.parkrangers.parkquest_backend.model.response.Park;
 import com.parkrangers.parkquest_backend.model.response.Subscription;
+import com.parkrangers.parkquest_backend.model.request.SubscriptionRequest;
 import com.parkrangers.parkquest_backend.repository.UserRepository;
 import com.parkrangers.parkquest_backend.service.SubscriptionService;
+import com.parkrangers.parkquest_backend.service.ParkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/subscriptions")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 public class SubscriptionController {
 
     @Autowired
@@ -22,13 +26,13 @@ public class SubscriptionController {
     // Create a new subscription for a user to a park
     @PostMapping("/subscribe")
     public ResponseEntity<Subscription> addSubscription(@RequestBody SubscriptionRequest request) {
-        System.out.println("Request received: userId=" + request.getUserId() + ", parkCode=" + request.getParkCode());
-
         Subscription subscription = subscriptionService.createSubscription(request.getUserId(), request.getParkCode());
         if (subscription != null) {
             return ResponseEntity.ok(subscription);
         } else {
             return ResponseEntity.badRequest().build();
         }
+
+
     }
 }
